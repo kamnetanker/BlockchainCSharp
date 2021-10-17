8078dcc7a6acd9c3ee2714a5bb0a5f98ea828215
kamnetanker <kamnetanker3@gmail.com>
2021-10-17 19:09:32 +0300

    init visual studio

---
+++ Solutions/BlockchainSLN/Blockchain1.B/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Blockchain1.B
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
